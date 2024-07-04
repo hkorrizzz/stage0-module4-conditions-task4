d72b939df0bef6866ef13bbecd89aacff71aadb0
hkorrizzz <tutvse253@gmail.com>
2024-07-04 14:31:56 +0300

    d

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        // Использование тройного условия для определения наибольшего числа
+        int max = first > second ? (first > third ? first : third): (second > third ? second : third);
+
+        // Вывод наибольшего числа
+        System.out.println(max);
+
     }
 }
